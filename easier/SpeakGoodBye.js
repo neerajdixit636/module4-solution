ccc9baec86010aec1b077b2214ea2d1b22c832bd
neerajdixit636 <neerajdixit@gmail.com>
2026-05-23 01:48:40 +0530

    sdf

--- easier/SpeakGoodBye.js
+++ easier/SpeakGoodBye.js
@@ -20,6 +20,15 @@ function speak(name) {
   console.log(speakWord + " " + name);
 }
 
+(function (window) {
+  var byeSpeaker = {} ;
+  byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  } ;
+  window.byeSpeaker = byeSpeaker ;
+}) (window) ;
+
+
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
 // xxxx.xxxx = byeSpeaker;
