b90db8b276afd0d44c1ec4b5c765e78d53924e09
neerajdixit636 <neerajdixit@gmail.com>
2026-05-23 01:41:35 +0530

    Enable logging for byeSpeaker in script.jsdfs
    Uncomment console log for byeSpeaker in script.js

--- easier/script.js
+++ easier/script.js
@@ -58,8 +58,8 @@ for (var i = 0; i < names.length; i++) {
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
   if (firstLetter === 'j') {
-    byeSpeaker.speak(names[i]);
-    // console.log("byeSpeaker " + names[i])
+    // byeSpeaker.speak(names[i]);
+    console.log("byeSpeaker " + names[i])
   } else {
     helloSpeaker.speak(names[i]);
   }
