f00cd2a82bb0408c222806ecb83a59a96d20e56b
neerajdixit636 <neerajdixit@gmail.com>
2026-05-23 01:45:35 +0530

    ads

--- easier/SpeakHello.js
+++ easier/SpeakHello.js
@@ -20,7 +20,7 @@ var speakWord = "Hello";
 
 (function (window) {
   var helloSpeaker = {} ;
-  helloSpeaker.speak = function () {
+  helloSpeaker.speak = function (name) {
     console.log(speakWord + " " + name);
   } ;
   window.helloSpeaker = helloSpeaker
