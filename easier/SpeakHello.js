d3e7e8fa93e4ff5fffafb81b97e6397ddfdb2247
neerajdixit636 <neerajdixit@gmail.com>
2026-05-23 01:15:58 +0530

    pdf

--- easier/SpeakHello.js
+++ easier/SpeakHello.js
@@ -22,3 +22,5 @@ function speak(name) {
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
 // xxxx.xxxx = helloSpeaker;
+
+// window.helloSpeaker = speak(name) ;
